ed2617bb700e478932878edfc76e9439f81e2ef0
Hamza Bishira <bishira12@hotmail.co.uk>
2020-02-25 13:44:29 +0000

    latest update

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let theMove = document.querySelector('#myMove')
+
+  theMove.addEventListener('click',function(){
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + "px";
+        elem.style.left = pos + "px";
+      }
+    }
+  });
